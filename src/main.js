991a1bee371b664446b2b6c328aa44f1c1020bee
Betchika99 <dobrl2011@mail.ru>
2019-07-22 21:45:51 +0300

    [ADDED] global function create

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import NumberGroupComponent from './components/NumberGroupComponent/NumberGroupComponent';
 import './main.scss';
 
-const props = {
-    mask: '+7(985)0II-**-**',
-};
-
-const num = new NumberGroupComponent(props);
-
-num.render(document.getElementById('application'));
-// document.getElementById('application').appendChild(num.render());
+export function create(props, container) {
+    const num = new NumberGroupComponent(props);
+    num.render(container);
+}
